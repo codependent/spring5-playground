a8ec45cdb6aa0615608ba26acb5860a5965ffa38
Jose Antonio Iñigo Garrido <jainigo@profile.es>
2016-08-02 12:30:02 +0200

    initial commit

---
+++ src/main/java/com/codependent/spring5/playground/Spring5PlaygroundApplication.java
@@ -0,0 +1,12 @@
+package com.codependent.spring5.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5PlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5PlaygroundApplication.class, args);
+	}
+}
