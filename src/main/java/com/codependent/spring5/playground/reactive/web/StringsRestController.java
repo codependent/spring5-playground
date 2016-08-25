42ca1b6a8372d7e02565b11e202e2262e9b8b35a
Jose Antonio Iñigo Garrido <jainigo@profile.es>
2016-08-25 14:30:14 +0200

    Publisher complies with spec

--- src/main/java/com/codependent/spring5/playground/reactive/web/StringsRestController.java
+++ src/main/java/com/codependent/spring5/playground/reactive/web/StringsRestController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.deser.std.NullifyingDeserializer;
+
 import reactor.core.publisher.Flux;
 
 @RestController
@@ -38,36 +40,7 @@ public class StringsRestController {
 	
 	@GetMapping(value="/strings", produces="text/event-stream")
     public Publisher<String> getStrings(){
-        return new Publisher<String>() {
-        	
-        	private int loops = 5;
-        	
-			@Override
-			public void subscribe(Subscriber<? super String> s) {
-				
-				s.onSubscribe(new Subscription() {
-					@Override
-					public void request(long n) {
-						for (int i = 0; i < n; i++) {
-							if(loops-- > 0){
-								try {
-									Thread.sleep(1000);
-								} catch (InterruptedException e) {
-									e.printStackTrace();
-								}
-								s.onNext("message"+Math.random());							
-							}else{
-								s.onComplete();
-							}
-						}
-					}
-					@Override
-					public void cancel() {
-						loops = 0;
-					}
-				});
-			}
-        };
+        return new StringsPublisher(5);
 	}
 	
 	@RequestMapping("/sse/event")
@@ -80,4 +53,99 @@ public class StringsRestController {
             return event;
         }).take(2);
     }
+	
+	class StringsPublisher implements Publisher<String>{
+
+		private int numberOfStrings;
+		
+		public StringsPublisher(int numberOfStrings){
+			this.numberOfStrings = numberOfStrings;
+		}
+		
+		@Override
+		public void subscribe(Subscriber<? super String> s) {
+			s.onSubscribe(new StringsSubscription(s));
+		}
+		
+		class StringsSubscription implements Subscription{
+			
+			private Subscriber<? super String> subscriber;
+			
+			public StringsSubscription(Subscriber<? super String> s){
+				this.subscriber = s;
+			}
+			
+			@Override
+			public void request(long n) {
+				if(numberOfStrings>0){
+					boolean completed = false;
+					for (long i = 0; i < n && !completed; i++) {
+						System.out.println(numberOfStrings);
+						if(numberOfStrings-- > 0){
+							try {
+								Thread.sleep(1000);
+							} catch (InterruptedException e) {
+								e.printStackTrace();
+							}
+							subscriber.onNext("message"+Math.random());		
+						}
+						if(numberOfStrings <= 0){
+							subscriber.onComplete();
+							completed = true;
+						}
+					}
+				}
+			}
+
+			@Override
+			public void cancel() {
+				numberOfStrings = 0;
+			}
+		}
+	}
+	
+	class StringsSubscriber implements Subscriber<String>{
+
+		private Subscription subscription;
+		
+		@Override
+		public void onSubscribe(Subscription s) {
+			this.subscription = s;
+			//s.request(Long.MAX_VALUE);
+		}
+
+		@Override
+		public void onNext(String t) {
+			System.out.println(t);
+		}
+
+		@Override
+		public void onError(Throwable t) {
+			t.printStackTrace();
+		}
+
+		@Override
+		public void onComplete() {
+			System.out.println("onComplete");
+		}
+		
+		public void request(long n){
+			subscription.request(n);
+		}
+		
+	}
+	
+	public static void main(String[] args) throws InterruptedException {
+		StringsRestController rsc = new StringsRestController();
+		
+		Publisher<String> publisher = rsc.new StringsPublisher(2);
+		StringsSubscriber subscriber = rsc.new StringsSubscriber();
+		publisher.subscribe(subscriber);
+		
+		subscriber.request(4);
+		subscriber.request(1);
+		subscriber.request(1);
+		
+		Thread.sleep(5000);
+	}
 }
